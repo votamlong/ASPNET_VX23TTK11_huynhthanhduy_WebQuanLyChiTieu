e7c9aa2dbd2261cc0994a59e25cf3321e0f8f278
votamlong <votamlong@gmail.com>
2025-09-16 11:07:37 +0700

    Add files via upload

---
+++ scr/ChiTieu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebQuanLyChiTieu.Models
+{
+    public class ChiTieu
+    {
+        public int Id { get; set; }
+        public DateTime Ngay { get; set; }
+        public string NoiDung { get; set; }
+        public decimal SoTien { get; set; }
+    }
+}
